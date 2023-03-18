be3518a149d8ae3a25342df845602ca55e381057
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2023-03-17 20:31:21 -0400

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo2.png';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo" 
         alt="logo" />
-        <a href="#" className="btn btn-primary shadow">
-          Hello</a>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small> 
+        Coded by Appollonia Khan
+        </small>
+      </footer>
+    </div>
     </div>
   );
 }
