783f90edbb5527fbfff299ba564c40c707ace454
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2023-03-20 22:42:43 -0400

    Added API Key

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,23 +8,27 @@ export default function Dictionary(props) {
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
-
-function handleResponse(response) {
+function handleDictionaryResponse(response) {
   setResults(response.data[0]);
+}
+
+function handlePexelsResponse(response) {
+  console.log(response);
 }
 
 function search() {
   let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-  axios.get(apiUrl).then(handleResponse);
-  }
+  axios.get(apiUrl).then(handleDictionaryResponse);
+  
+  let pexelsApiKey = "EADLQmaWSMtpa3FvI2FiZNJhJADhWq1S1tFXgMrBeS4XE1D2CuifHrbx";
+  let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+  let headers = {Authorization: `Bearer ${pexelsApiKey}` }
+  axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+}
 
 function handleSubmit(event) {
   event.preventDefault();
   search();
-}
-
-function handleKeywordChange(event) {
-  setKeyword(event.target.value);
 }
 
 function handleKeywordChange(event) {
