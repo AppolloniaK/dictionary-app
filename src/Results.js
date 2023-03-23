c4ca518c14a53d4237c27f5aa93d5f4164e86d28
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2023-03-22 21:01:59 -0400

    Updated all css

--- src/Results.js
+++ src/Results.js
@@ -9,15 +9,15 @@ return (
 <div className="Results">
     <section>  
     <h2>{props.results.word}</h2>
-    {props.results.phonetics.map(function(phonetic, index) {
+    {props.results.phonetics.map(function (phonetic, index) {
 return (
     <div key={index}>
         <Phonetic phonetic={phonetic} />
         </div>
-)
+);
     })}
 </section>
-    {props.results.meanings.map(function(meaning, index) {
+    {props.results.meanings.map(function (meaning, index) {
         return (
             <section key={index}>
                 <Meaning meaning={meaning} />
